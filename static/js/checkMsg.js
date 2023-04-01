34afbeaf6fdf10e29fe91b47b242ae74b69d86f0
yong1le <ygle.he@gmail.com>
2023-03-31 20:49:48 -0400

    merge related scripts

--- static/js/checkMsg.js
+++
@@ -1,9 +0,0 @@
-const msg = document.querySelector('#new-msg');
-const form = document.querySelector('.msg-form');
-
-const checkEmpty = (e) => {
-  if (msg.value === '')
-    e.preventDefault(); 
-}
-
-form.addEventListener('submit', checkEmpty);
