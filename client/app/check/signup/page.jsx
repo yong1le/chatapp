e53433aa93a07ca717077efd87688567facd8479
yong1le <ygle.he@gmail.com>
2023-08-18 16:37:12 -0400

    fix broken imports

--- client/app/check/signup/page.jsx
+++ client/app/check/signup/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { navigateToPage } from "@/api/Navigation";
+import { setUser } from "@/api/Session";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
